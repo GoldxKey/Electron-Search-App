c50490f1964d165600f7ab02c0e77492aac3f8f6
ecmadao <wlec@outlook.com>
2016-05-18 09:06:51 +0800

    增加store缓存

--- app/Component/index/index.js
+++ app/Component/index/index.js
@@ -4,18 +4,27 @@ import Dashboard from '../Dashboard/index';
 
 import { createStore, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
 import {gitSearchApp} from '../../Redux/reducers/index';
 
 require('../../Page/stylesheet/index.css');
 
-const store = createStore(
-    gitSearchApp,
-    applyMiddleware(thunkMiddleware)
-);
+import {persistStore, autoRehydrate} from 'redux-persist';
+
+const middlewareWrapper = applyMiddleware(thunk);
+const store = middlewareWrapper(createStore);
+
+const rehydrator = autoRehydrate();
+const AppStore = rehydrator(store)(gitSearchApp);
+persistStore(AppStore);
+
+// const AppStore = createStore(
+//     gitSearchApp,
+//     applyMiddleware(thunk)
+// );
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={AppStore}>
     <Dashboard />
   </Provider>,
   document.getElementById('main_component')
