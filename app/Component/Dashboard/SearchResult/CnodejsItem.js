667acdecd5c6b703aab7a38868c8233cad539f4e
ecmadao <wlec@outlook.com>
2016-05-27 01:20:20 +0800

    css优化

--- app/Component/Dashboard/SearchResult/CnodejsItem.js
+++ app/Component/Dashboard/SearchResult/CnodejsItem.js
@@ -8,44 +8,25 @@ require('../../../Page/stylesheet/SearchItem/cnodejs_item.less');
 class CnodejsItem extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      expand: false
-    }
-  }
-
-  handleExpandChange() {
-    let {expand} = this.state;
-    this.setState({
-      expand: !expand
-    });
   }
 
   render() {
-    let {expand} = this.state;
     let {item} = this.props;
-    let {title, content, create_at} = item;
-
-    let expandIcon = className('expand_icon fa', {
-      'fa-angle-down': !expand,
-      'fa-angle-up': expand
-    });
-
-    let topContentClass = className('top_content', {
-      expand: expand
-    });
+    let {title, content, create_at, reply_count} = item;
 
     return (
       <div className="cnodejs_item_container">
         <div className="cnodejs_top">
           <div className="top_title">{title}</div>
-          <div className={topContentClass}>{content}</div>
+          <div className="top_content">{content.slice(0, 101)}</div>
         </div>
         <div className="cnodejs_bottom">
           <div className="bottom_info">
-            <i className="fa fa-calendar" aria-hidden="true"></i>&nbsp;<span>{create_at.split('T')[0]}</span>
+            <i className="fa fa-calendar" aria-hidden="true"></i>&nbsp;&nbsp;<span>{create_at.split('T')[0]}</span>
           </div>
-          <div className="bottom_expand">
-            <i className={expandIcon} aria-hidden="true" onClick={this.handleExpandChange.bind(this)}></i>
+          <div className="bottom_replay">
+            <i className="replay_icon fa fa-comments-o" aria-hidden="true"></i>
+            &nbsp;&nbsp;<span>{reply_count}</span>
           </div>
         </div>
       </div>
