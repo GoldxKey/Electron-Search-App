f9add8c9fe7a3aca6f94b6d70eaf418ea877acfe
ecmadao <wlec@outlook.com>
2016-05-27 09:18:53 +0800

    test mocha test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function() {
       [1,2,3].indexOf(4).should.equal(-1);
       [1,2,3].indexOf(0).should.equal(-1);
+      [1,2,3].indexOf(1).should.equal(0);
     });
   });
 });
